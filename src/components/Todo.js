01557d8f560123c94db1827e4011f2816b2721f0
keebon <keebon@.github.com>
2019-05-13 22:53:54 +0800

    updated todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,18 +5,19 @@ import TodoList from './TodoList';
 export default class Todo extends React.Component{
     state = {
         todos : [],
-        todoShow : "all"
+        todoShow : "all",
+        toggleAllComplete : true
     }
 
     addTodo = (todo) => {
-        this.setState({
-            todos : [todo, ...this.state.todos]
-        });
+        this.setState(state => ({
+            todos : [todo, ...state.todos]
+        }));
     }
 
     handleToggle = (id) => {
-        this.setState({
-            todos : this.state.todos.map(todo => {
+        this.setState(state => ({
+            todos : state.todos.map(todo => {
                 if(todo.id === id){
                     return{
                         ...todo,
@@ -26,13 +27,35 @@ export default class Todo extends React.Component{
                     return todo;
                 }
             })
-        });
+        }));
     }
 
     toggleShow = (e) => {
         this.setState({
             todoShow : e.target.name
         });
+    }
+
+    handleDelete = (id) => {
+        this.setState(state => ({
+            todos : state.todos.filter(todo => todo.id !== id)
+        }));
+    }
+
+    removeTodosThatAreComplete = () => {
+        this.setState(state => ({
+            todos : state.todos.filter(todo => !todo.completed)
+        }));
+    }
+
+    handleToggleAllComplete = () => {
+        this.setState(state => ({
+            todos : state.todos.map(todo => ({
+                ...todo,
+                completed : state.toggleAllComplete
+            })),
+            toggleAllComplete : !state.toggleAllComplete
+        }));
     }
 
     render(){
@@ -55,11 +78,22 @@ export default class Todo extends React.Component{
                     key = {todo.id}
                     todo = {todo}
                     toggleComplete = {() => this.handleToggle(todo.id)}
+                    delete = {() => this.handleDelete(todo.id)}
                     />
                 )}
+                <div>Todos Left : {this.state.todos.filter(todo => !todo.completed).length}</div>
                 <button name = "all" onClick = {this.toggleShow}>All</button>
                 <button name = "active" onClick = {this.toggleShow}>Active</button>
                 <button name = "completed" onClick = {this.toggleShow}>Completed</button>
+                {this.state.todos.some(todo => todo.completed) ? 
+                (
+                    <div>
+                        <button onClick = {this.removeTodosThatAreComplete}>Remove All Completed</button>
+                    </div>
+                ) : null }
+                <div>
+                    <button onClick = {this.handleToggleAllComplete}>Toggle All Complete : {`${this.state.toggleAllComplete}`}</button>
+                </div>
             </div>
         );
     };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 export default props => (
     <div style = {{
+        display : "flex", justifyContent : "center"
+    }}>
+    <div style = {{
         textDecoration : props.todo.completed ? "line-through" : null,
         cursor : "pointer"
     }} onClick = {props.toggleComplete}>{props.todo.text}</div>
+    <button onClick = {props.delete}>X</button>
+    </div>
 )
