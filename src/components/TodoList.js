a9b9cc70f4334d876f44fdea5e1ed306a3a3d373
Lois Kevin Datugan <41941511+keebon@users.noreply.github.com>
2019-05-13 22:55:30 +0800

    Delete TodoList.js

--- src/components/TodoList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default props => (
-    <div style = {{
-        textDecoration : props.todo.completed ? "line-through" : null,
-        cursor : "pointer"
-    }} onClick = {props.toggleComplete}>{props.todo.text}</div>
-)
